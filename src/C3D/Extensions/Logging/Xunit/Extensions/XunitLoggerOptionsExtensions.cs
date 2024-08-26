4f934a9335df58245cda25ef1e3b3aa70aaf9926
Cynthia Z E MacLeod (C3D) <cynthia.macleod@cloud3d.ltd>
2024-08-26 18:49:17 +0100

    Add fluent configuration options

---
+++ src/C3D/Extensions/Logging/Xunit/Extensions/XunitLoggerOptionsExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace C3D.Extensions.Xunit.Logging;
+
+public static class XunitLoggerOptionsExtensions { 
+    public static XunitLoggerOptions WithTimeStamp(this XunitLoggerOptions options, XunitLoggerTimeStamp timeStamp)
+    {
+        options.TimeStamp = timeStamp;
+        return options;
+    }
+
+    public static XunitLoggerOptions WithTimeStampFormat(this XunitLoggerOptions options, string timeStampFormat)
+    {
+        options.TimeStampFormat = timeStampFormat;
+        return options;
+    }
+
+    public static XunitLoggerOptions UseTimeProvider(this XunitLoggerOptions options, TimeProvider timeProvider)
+    {
+        options.GetUtcNow = timeProvider.GetUtcNow;
+        return options;
+    }
+
+    public static XunitLoggerOptions Restart(this XunitLoggerOptions options)
+    {
+        options.LogStart = options.GetUtcNow();
+        return options;
+    }
+
+    public static XunitLoggerOptions UseCulture(this XunitLoggerOptions options, CultureInfo culture)
+    {
+        options.Culture = culture;
+        return options;
+    }
+}
